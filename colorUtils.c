6c07871e59bf0902c491c59ca318a8c395329b53
murtazatanveer <murtaza.tanveer488@gmail.com>
2023-12-21 19:29:03 +0500

    implementing toSepia(); function

--- colorUtils.c
+++ colorUtils.c
@@ -16,5 +16,12 @@ int toGrayScale(int *r, int *g, int *b, Mode mode) {
 }
 
 int toSepia(int *r, int *g, int *b) {
-  //TODO: implement
+  
+   *r =  (0.393 * *r) + (0.769 * *g) + (0.189 * *b);
+
+   *g =  (0.349 * *r ) + (0.686 * *g) + (0.168 * *b);
+
+   *b = (0.272 * *r) + (0.534 * *g) + (0.131 * *b);
+
+
 }
